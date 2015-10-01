24b2d70aa352bf4ca73f4aa38318840d5e4f74bd
FangJuping <juping61@gmail.com>
2015-10-01 18:03:11 +0800

    Storage-juping

--- TaskApp/src/Storage.java
+++ TaskApp/src/Storage.java
@@ -1,4 +1,11 @@
-//testing
+/**
+ * The Storage saves all the data of the user,
+ * and will be updated according to commands, i.e. add/delete/update.
+ * @author fang
+ */
+import java.lang.*;
+
 public class Storage {
+	
 
 }
