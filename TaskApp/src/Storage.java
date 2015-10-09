d8320530a6974aa74538d0ce16f5275c35d5089d
FangJuping <juping61@gmail.com>
2015-10-09 10:36:27 +0800

    Storage-juping

--- TaskApp/src/Storage.java
+++ TaskApp/src/Storage.java
@@ -4,9 +4,34 @@ package src;
  * and will be updated according to commands, i.e. add/delete/update.
  * @author fang
  */
-import java.lang.*;
+import java.io.*;
 
 public class Storage {
+	public Storage(){
+		
+	}
 	
+	static final Boolean OVERWRITE_TO_FILE = false;
+	
+	static void createSaveFile (String fileName) {
+		File file = new File(fileName);
+		if (!file.exists()) {
+		   writeTextToFile(file, "", Storage.OVERWRITE_TO_FILE );
+		}
+		//Write to existing file.
+	}
+	
+	static void writeTextToFile (File file, String task, Boolean isAppend) {
+		OutputStream output = null;
+		try {
+			output = new FileOutputStream(file, isAppend);
+			output.write(task.getBytes());
+			if (isAppend == true) {
+			    output.write('\n');
+			    }
+			} catch (IOException e) {
+				e.printStackTrace();
+				}
+		}
+	}
 
-}
