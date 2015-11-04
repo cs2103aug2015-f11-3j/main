6ff0d77d2c6eadb13259a7a4fa1c4215b29ce8a1
cuiyichong <CYC@Yichongs-MacBook-Air.local>
2015-11-05 00:01:57 +0800

    parser - make reoccurring with n =1

--- TaskApp/src/TaskManager.java
+++ TaskApp/src/TaskManager.java
@@ -3,12 +3,15 @@ package src;
 public class TaskManager {
 //test
 	public static void main(String[] arg) {
+		
 		CommandParser cp = new CommandParser();
-		String str ="add dojhshdhj 11/12/2015";
+		System.out.println(cp.isNumeric("700"));
+		String str ="add dojhshdhj every mon until 12 dec 2015";
 		Command cmd = cp.parse(str);
 		System.out.println(cmd.getCommandType());
 		System.out.println(cmd.getTask());
 		System.out.println(cmd.getDates().toString());
+		System.out.println(cmd.getKey());
 		
 	}
 }
