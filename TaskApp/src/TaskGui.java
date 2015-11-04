ad8a7fbedb4f784272ee987b867660a3e60abb07
ZhouCihang <matthewzhou1128@gmail.com>
2015-11-05 01:26:45 +0800

    V3.5.3 GUI Updated

--- TaskApp/src/TaskGui.java
+++ TaskApp/src/TaskGui.java
@@ -22,6 +22,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.paint.Color;
@@ -36,13 +37,15 @@ public class TaskGui extends Application{
 	private static final String ROOT_LAYOUT_FXML = "/layout/RootLayout.fxml";
 	private static final String WINDOW_TITLE = "Task Manager";
 	private static final String CONTAINER_LAYOUT_FXML = "/layout/Container.fxml";
-	private BorderPane root;
+	private AnchorPane root;
 	private GridPane container;
 	private Stage primaryStage;
 	private ListViewController listViewController;
 	private UpdateTableController updateTableController;
 	private TodoTableController todoTableController;
 	private OverdueTableController overdueTableController;
+	//private CommandBarController commandBarController;
+	
 	//private TaskLogic taskLogic;
 	private Logic logic;
 	
@@ -61,9 +64,9 @@ public class TaskGui extends Application{
 		initialLogic();
 		addCommandBar(this);
 		addListView();
-		addTableView();
-		addTableView1();
-		addTableView2();
+		//addTableView();
+		//addTableView1();
+		//addTableView2();
 	}
 	
 	private void addTableView2() {
@@ -120,25 +123,7 @@ public class TaskGui extends Application{
 		return data;
 	}
 	
-	private ObservableList<Message> getInitialTableData3(ArrayList<Tasks> listTask) {
-		// TODO Auto-generated method stub
-		List<Message> list = new ArrayList<Message>();
-		listTask = new ArrayList<Tasks>();
-		//Tasks task = new Tasks(null, null);
-		//list.add(new Message(1, "homework", "Sat"));
-		//list.add(new Message(1, "homework", "Sat"));
-		
-		for(int i=0; i<listTask.size(); i++){
-			list.add(new Message(listTask.get(i).getIndex(), listTask.get(i).getEvent(),
-					listTask.get(i).getDate().get(listTask.get(i).getDate().size()-1).toString()));
-		}
-		//ObservableList<Message> data = FXCollections.observableArrayList(list);
-		data = FXCollections.observableArrayList(list);
-		
-		return data;
-	}
-	
-	private void addTableView() {
+	/*private void addTableView() {
 		// TODO Auto-generated method stub
 		
 		data = getInitialTableData();
@@ -146,7 +131,7 @@ public class TaskGui extends Application{
 		//listViewController.getChildren().add(updateTableController);
 		//root.setCenter(updateTableController);
 		container.add(updateTableController, 1, 0);
-	}
+	}*/
 
 	private void initialLogic() {
 		// TODO Auto-generated method stub
@@ -158,13 +143,18 @@ public class TaskGui extends Application{
 		// TODO Auto-generated method stub
 		//root.setLeft(new ListViewController(userInput));
 		listViewController = new ListViewController();
-		container.add(listViewController, 0, 0);
+		root.getChildren().add(listViewController);
+		AnchorPane.setTopAnchor(listViewController, 10.0);
+		AnchorPane.setLeftAnchor(listViewController, 15.0);
+		AnchorPane.setBottomAnchor(listViewController, 70.0);
+		
+		//container.add(listViewController, 0, 0);
 		//root.setTop(listViewController);
 	}
 
 	private void addCommandBar(TaskGui taskGui) {
 		// TODO Auto-generated method stub
-		root.setBottom(new CommandBarController(taskGui));
+		/*root.setBottom(new CommandBarController(taskGui));
 		
 		FXMLLoader contain = new FXMLLoader(getClass().getResource(CONTAINER_LAYOUT_FXML));
 		try {
@@ -173,7 +163,13 @@ public class TaskGui extends Application{
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-        root.setCenter(container);
+        root.setCenter(container);*/
+		CommandBarController commandBarController = new CommandBarController(taskGui);
+		root.getChildren().add(commandBarController);
+		AnchorPane.setBottomAnchor(commandBarController, 10.0);
+		AnchorPane.setLeftAnchor(commandBarController, 15.0);
+		AnchorPane.setRightAnchor(commandBarController, 15.0);
+		
 	}
 
 	private void initialPrimaryStage(Stage primaryStage) {
@@ -198,7 +194,7 @@ public class TaskGui extends Application{
 	public void handleKeyPress(CommandBarController commandBarController, KeyCode key, String userInput) {
 		
 		if (key == KeyCode.ENTER) {
-			System.out.println(userInput);
+			System.out.println(userInput);			
 			handleEnterPress(commandBarController, userInput);
 		}
 	}
@@ -208,11 +204,57 @@ public class TaskGui extends Application{
 		//addListView(userInput);
 		//listViewController.addItemToListView(taskLogic.executeCommand(userInput));
 		logic.executeCommand(userInput);
-		//System.out.println("$$$$$$$$$$"+ logic.getConsole().get(0));
-		//listViewController.addItemToListView(logic.getConsole().get(0));
 		listViewController.addArrayList(logic.getConsole());
-		
+		System.out.println("SIZE: "+logic.getToDoTask().size());
+		addTodoTableView(logic.getToDoTask());
+		addOverdueTableView(logic.getDiscard());
+		addSearchTableView(logic.getSearch());
 		commandBarController.clear();
+	}
+
+	private void addSearchTableView(ArrayList<Tasks> search) {
+		// TODO Auto-generated method stub
+		data2 = getInitialTableData3(search);
+		updateTableController = new UpdateTableController(data2);
+		root.getChildren().add(updateTableController);
+		AnchorPane.setTopAnchor(updateTableController, 25.0);
+		AnchorPane.setLeftAnchor(updateTableController, 950.0);
+		AnchorPane.setBottomAnchor(updateTableController, 70.0);
+		
+	}
+
+	private void addOverdueTableView(ArrayList<Tasks> discard) {
+		// TODO Auto-generated method stub
+		data1 = getInitialTableData3(discard);
+		overdueTableController = new OverdueTableController(data1);
+		root.getChildren().add(overdueTableController);
+		AnchorPane.setTopAnchor(overdueTableController, 25.0);
+		AnchorPane.setLeftAnchor(overdueTableController, 680.0);
+		AnchorPane.setBottomAnchor(overdueTableController, 70.0);
+		
+	}
+
+	private void addTodoTableView(ArrayList<Tasks> toDoTask) {
+		// TODO Auto-generated method stub
+		data = getInitialTableData3(toDoTask);
+		todoTableController = new TodoTableController(data);
+		root.getChildren().add(todoTableController);
+		AnchorPane.setTopAnchor(todoTableController, 25.0);
+		AnchorPane.setLeftAnchor(todoTableController, 410.0);
+		AnchorPane.setBottomAnchor(todoTableController, 70.0);
+	}
+	
+	private ObservableList<Message> getInitialTableData3(ArrayList<Tasks> listTask) {
+		// TODO Auto-generated method stub
+		List<Message> list = new ArrayList<Message>();
+		System.out.println("!@#$%^& "+ listTask.size());
+		for(int i=0; i<listTask.size(); i++){
+			System.out.println(listTask.get(i).getDate().toString() + "	@@@@@");
+			list.add(new Message(listTask.get(i).getIndex(), listTask.get(i).getEvent(),
+					listTask.get(i).getDate().toString()));
+		}
+		data = FXCollections.observableArrayList(list);
+		return data;
 	}
 
 	public static void main(String[] args) {
@@ -223,122 +265,3 @@ public class TaskGui extends Application{
 		}
 	}
 }
-
-
-
-
-
-
-
-
-
-/*
-public class TaskGui extends Application {
-	
-	TextField textfield;
-	BorderPane rootlayout;
-	Text actiontarget;
-	
-	ListView<String> console;
-	
-	TaskLogic tasklogic;
-	
-	private static Logger logger = Logger.getLogger("Foo");
-
-	@Override
-	public void start(Stage primaryStage) throws Exception {
-<<<<<<< Updated upstream
-
-		tasklogic = new TaskLogic();
-=======
-		// TODO Auto-generated method stub
->>>>>>> Stashed changes
-		
-		logger.log(Level.INFO, "going to start processing");
-		
-		tasklogic = new TaskLogic();
-		primaryStage.setTitle("TaskManager");
-		
-		Text scenetitle = new Text("Welcome to TaskManager!");
-		scenetitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
-		//---------test-------
-		final ListView<String> console = new ListView<String>(FXCollections.<String>observableArrayList());
-        // listen on the console items and remove old ones when we get over 10 items
-        console.getItems().addListener(new ListChangeListener<String>() {
-            @Override 
-            public void onChanged(Change<? extends String> change) {
-                while (change.next()) {
-                    if (change.getList().size() > 20) 
-                    	change.getList().remove(0);
-                }
-            }
-        });
-        
-        // create text box for typing in
-        actiontarget = new Text();
-        textfield = new TextField();
-        textfield.setPromptText("Write you commands here.");
-        textfield.setStyle("-fx-font-size: 25;");
-        
-        tasklogic = new TaskLogic();
-        
-        //add a key listenersa
-        textfield.setOnKeyPressed(new EventHandler<KeyEvent>() {
-            public void handle(KeyEvent ke) {
-            	//assert ke.getCode() == KeyCode.ENTER: "correct";
-            	if(ke.getCode() == KeyCode.ENTER){
-
-            		handleEnterPress();
-            		textfield.clear();
-
-            	}
-            }
-
-			private void handleEnterPress() {
-
-<<<<<<< Updated upstream
-=======
-				// TODO Auto-generated method stub
-				logger.log(Level.INFO, "end of processing");
-				assert tasklogic.executeCommand(textfield.getText()).equals("Added task do homework for Fri, 16 Oct"): "message correct";
-				
->>>>>>> Stashed changes
-				console.getItems().add("Key Pressed: " + tasklogic.executeCommand(textfield.getText()));
-				//assertEquals("add do homework", tasklogic.executeCommand(textfield.getText()));
-				
-				
-				actiontarget.setFill(Color.FIREBRICK);
-                actiontarget.setText("thanks for type in command.\n"+textfield.getText());
-			}
-        });
-		
-		rootlayout = new BorderPane();
-		//rootlayout.setRight(actiontarget);
-		rootlayout.setBottom(textfield);
-		rootlayout.setTop(scenetitle);
-		rootlayout.setCenter(console);
-		
-
-		Scene scene = new Scene(rootlayout, 700, 450);
-
-		primaryStage.setScene(scene);
-		primaryStage.show();
-		
-	}
-	
-	public static void main(String[] args) {
-		try{
-        launch(args);
-		}catch(Throwable e){
-			System.out.println(e.getMessage());
-		}
-	}
-}*/
-
-
-
-
-
-
-
-
